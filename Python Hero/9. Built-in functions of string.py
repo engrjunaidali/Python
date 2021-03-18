8add36eb763cf255997d28cf8f63cedfd7ff98e1
engrjunaidali <80405587+engrjunaidali@users.noreply.github.com>
2021-03-18 20:58:24 +0500

    Add files via upload

---
+++ Python Hero/9. Built-in functions of string.py
@@ -0,0 +1,25 @@
+x="where were you yesterday"
+
+print("lowercase",x.lower())
+print("UPPERCASE",x.upper())
+print("TitleCase",x.title())
+print(x.capitalize())
+
+print(x.split())
+print(x.split("s"))
+
+print(x.splitlines())
+
+print("count : ",x.count("i"))
+print("find : ",x.find("i"))
+
+print(x.replace("is","was"))
+
+print(x.casefold())
+
+print(x.endswith("end"))
+
+print(x.expandtabs())
+
+print(x.join("is"))
+
