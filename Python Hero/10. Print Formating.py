9e487bb93c14520aad1e39983622ba4266be836d
engrjunaidali <80405587+engrjunaidali@users.noreply.github.com>
2021-03-18 20:58:41 +0500

    Add files via upload

---
+++ Python Hero/10. Print Formating.py
@@ -0,0 +1,12 @@
+email="HDB, this is from Junaid"
+print(email,email,sep="\n")
+
+guest="John"
+host="Junaid"
+
+print("Hey, HBD {0}, this is from {1}".format(guest,host))
+print(f"Hey, HBD {guest}, this is from {host}")
+
+a="Juniad"
+b="17"
+print ("Hello World, my name is %s and I am %s yearsold"%(a,b))
